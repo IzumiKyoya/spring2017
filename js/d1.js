1a752ea9322b99f4703f7a4862a3cde01736a6ac
IzumiKyoya <ri5856@yahoo.co.jp>
2017-02-27 15:06:39 +0900

    第１回課題提出

---
+++ js/d1.js
@@ -0,0 +1,13 @@
+﻿var tex = (function (){
+	console.log("これはＪＳのサンプルです");
+	})();
+	
+	var cat = function (name){
+	this.name = name;
+	this.run = function(){
+	console.log(this.name +"が走る");
+	};
+	};
+	
+	var bag = new cat("たま");
+	bag.run()
